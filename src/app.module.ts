49ca3eddc0ebf5eea9b4ea413051a6c7dce43bf8
Sinaf19 <quentinsurdez@gmail.com>
2024-08-25 11:55:31 +0200

    connection to locahost db successful

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'test',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,5 @@
 export class CreateItemDto {
-    id: number;
-    name: string;
-    price: number;
+  id: number;
+  name: string;
+  price: number;
 }
