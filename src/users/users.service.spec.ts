1b32a94101bbf81a12fa369591c849b79d64919d
Sinaf19 <quentinsurdez@gmail.com>
2024-08-28 11:47:15 +0200

    tests passed

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -15,16 +15,27 @@
 
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
