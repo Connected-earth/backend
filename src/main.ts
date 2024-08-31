33de92cc7ce7235fc14f7733c0c8462c4e21fb54
Sinaf19 <quentinsurdez@gmail.com>
2024-08-31 14:16:13 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
