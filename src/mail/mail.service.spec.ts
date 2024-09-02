d2f296b5087ddf2704e24a361b6e104df71871eb
Sinaf19 <quentinsurdez@gmail.com>
2024-09-02 17:11:39 +0200

    tests passed

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 describe('MailService', () => {
   let service: MailService;
+  let mailerService: MailerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailService],
+      providers: [
+        MailService,
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn().mockResolvedValue('Email sent successfully'),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MailService>(MailService);
+    mailerService = module.get<MailerService>(MailerService);
   });
 
   it('should be defined', () => {
