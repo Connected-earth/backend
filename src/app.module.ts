cfc52206a3f0e0ce783d2271f65ad0a9dbb12e10
sainane <rachel.tranchida@gmail.com>
2024-09-05 14:50:44 +0200

    fix config for vercel for mail template path 4.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -64,7 +64,7 @@ import { UserPlantsLinkedGeneralPlantsViewEntity } from './plants/userPlants/ent
         from: '"info@plantkeeper.ch" <info@plantkeeper.ch>',
       },
       template: {
-        dir: 'src/mail/templates/',
+        dir: process.cwd() + '/src/mail/templates/',
         adapter: new HandlebarsAdapter(), // or new PugAdapter()
         options: {
           strict: true,
